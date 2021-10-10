28c6a064666b6e811b3b1b5a4d08ad304e1e0242
Osixty <90504546+Osixty@users.noreply.github.com>
2021-10-10 17:09:37 +0700

    01 PHP

---
+++ 3_XIRPL1_12.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$nama = array("Satria"=>"Xi rpl 1","Arsyi"=>"Xi rpl2","Dhika"=>"Xi rpl 3","Evan"=> "xirpl4","fabian"=>"xirpl5","Irfan"=>"xirpl6"); 
+foreach($nama as $x => $x_value) {
+    echo "nama=" . $x . ", kelas=" . $x_value;
+    echo "<br>";
+  }
+?>
+
+</body>
+</html>
+    
---
+++ proses.php
@@ -0,0 +1,5 @@
+<?php
+    $nama = $_POST['Nama'];
+    $alamat = $_POST['Alamat'];
+    echo "Nama". $nama . "<br/>" . "Alamat:" . $alamat;
+?>
